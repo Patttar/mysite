8822d56c94fd42426d3410643fb87b3a66f2b453
Patttar <anton.karpuk.webdad@gmail.com>
2026-03-24 22:05:58 +0300

    fix: configure GitHub Pages deployment with HashRouter and GitHub Actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Case from './pages/Case';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/case" element={<Case />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
---
+++ запустить.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Запускаю сайт...
+cd /d "%~dp0"
+npm run dev
+pause
